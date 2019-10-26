0fd01515402e5266638a269d9c0b41465e2056f4
Niclas Jarowsky <jarowsky.niclas@web.de>
2019-10-26 16:09:03 +0200

    Seitenname geaendert

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <head>
 	<meta charset="UTF-8">
-	<title>Fachschaftsrat</title>
+	<title>Fachschaftsratseite</title>
 	<meta name="description" content="Kurzbeschreibung">
 	<link href="design.css" rel="stylesheet">
     <link rel="shortcut icon" type="image/png" href="assets/images/ailogo.png">
