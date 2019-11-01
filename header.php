c9b37af1529a2c54ccdbfde647c99f4052a80ced
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2019-11-01 12:18:36 +0100

    changed header delete submenue class added icon for submenue

--- header.php
+++ header.php
@@ -1,7 +1,23 @@
-<head>
-	<meta charset="UTF-8">
-	<title>Fachschaftsrat</title>
-	<meta name="description" content="Kurzbeschreibung">
-	<link href="design.css" rel="stylesheet">
-    <link rel="shortcut icon" type="image/png" href="assets/images/ailogo.png">
-</head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <nav>
+        <ul>
+            <li><a href="#" title="Startseite">Startseite</a></li>
+            <li><a href="#" title="Das sind wir">Über uns</a></li>
+            <li><a href="#" title="Unsere Veranstaltungen">Events</a></li>
+            <li><a href="#" title="">Mitglieder <i class="fa fa-caret-down"></i></a>
+                <ul>
+                    <li><a href="#" title="">Aktuelle Mitglieder</a>
+                    <li><a href="#" title="">Archivierte Mitglieder</a>
+                </ul>
+            </li>
+            <li><a href="#" title="">Fotos</a></li>
+            <li><a href="#" title="">Intern <i class="fa fa-caret-down"></i></a>
+                <ul>
+                    <li><a href="#" title="">Login</a>
+                    <li><a href="#" title="">Profil</a>
+                    <li><a href="#" title="">Nutzerverwaltung</a>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+
--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html lang="de">
-<head>
-	<?include "header.php";?>
-</head>
-<body>
-    <?include "footer.php";?>
-</body>
+    <head>
+        <title>Fachschaftsrat</title>
+        <meta charset="UTF-8">
+        <meta charset="UTF-8">
+        <meta name="description" content="Kurzbeschreibung">
+        <link rel="stylesheet" href="assets/css/design.css">
+        <link rel="shortcut icon" type="image/png" href="assets\images\ailogo.png">
+    </head>
+
+    <body>
+        <?include "header.php";?>
+        <?include "footer.php";?>
+    </body>
+
 </html>
