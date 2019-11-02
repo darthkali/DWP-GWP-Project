9af96ac6b60e84ed4c7819a30d554cd45123f6b5
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2019-11-02 14:10:32 +0100

    Code ein wenig aufgeräumt

--- navMenuBar.php
+++ navMenuBar.php
@@ -1,32 +1,27 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
 <nav>
-        <ul>
-            <li><a href="#" title="">Intern <i class="fa fa-caret-down"></i></a>
-                <ul>
-                    <li><a href="/FSAI-Site/pages/login.php" title="">Login</a>
-                    <li><a href="#" title="">Profil</a>
-                    <li><a href="#" title="">Nutzerverwaltung</a>
-                </ul>
-            </li>
-            <li><a href="#" title="">Mitglieder <i class="fa fa-caret-down"></i></a>
-                <ul>
-                    <li><a href="#" title="">Aktuelle Mitglieder</a>
-                    <li><a href="#" title="">Archivierte Mitglieder</a>
-                </ul>
-            </li>
-            <li><a href="#" title="">Fotos</a></li>
-            <li><a href="#" title="Unsere Veranstaltungen">Events</a></li>
-            <li><a href="/FSAI-Site/pages/vorlage.php" title="Das sind wir">Über uns</a></li>
-            <li><a href="/FSAI-Site/" title="Startseite">Startseite</a></li>
-        </ul>
-
+    <ul>
+        <li><a href="#" title="">Intern <i class="fa fa-caret-down"></i></a>
+            <ul>
+                <li><a href="/FSAI-Site/pages/login.php" title="">Login</a>
+                <li><a href="#" title="">Profil</a>
+                <li><a href="#" title="">Nutzerverwaltung</a>
+            </ul>
+        </li>
+        <li><a href="#" title="">Mitglieder <i class="fa fa-caret-down"></i></a>
+            <ul>
+                <li><a href="#" title="">Aktuelle Mitglieder</a>
+                <li><a href="#" title="">Archivierte Mitglieder</a>
+            </ul>
+        </li>
+        <li><a href="#" title="">Fotos</a></li>
+        <li><a href="#" title="Unsere Veranstaltungen">Events</a></li>
+        <li><a href="/FSAI-Site/pages/vorlage.php" title="Das sind wir">Über uns</a></li>
+        <li><a href="/FSAI-Site/" title="Startseite">Startseite</a></li>
+    </ul>
     <div id="NavContent" >
         <img src="/FSAI-Site/assets/images/ailogo_groß.png">
         Fachschaftsrat Angewandte Informatik
-
-
     </div>
-    </nav>
+</nav>
 
