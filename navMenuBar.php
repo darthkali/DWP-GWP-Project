10f6ee3985f8fbde629bbfe327ab80eca2866602
Niclas Jarowsky <jarowsky.niclas@web.de>
2019-11-03 19:02:52 +0100

    Datenschutz überarbeitet

--- navMenuBar.php
+++ navMenuBar.php
@@ -1,7 +1,5 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
 <nav>
-
     <ul>
         <li><a href="#" title="">Intern <i class="fa fa-caret-down"></i></a>
             <ul>
@@ -17,17 +15,15 @@
             </ul>
         </li>
         <li><a href="#" title="">Fotos</a></li>
-        <li><a href="#" title="Unsere Veranstaltungen">Events</a></li>
+        <li><a href="events.php" title="Unsere Veranstaltungen">Events</a></li>
         <li><a href="/FSAI-Site/pages/ueberUns.php" title="Das sind wir">Über uns</a></li>
        <!--  <li><a href="/FSAI-Site/" title="Startseite">Startseite</a></li> -->
     </ul>
-
     <div id="NavContent">
         <a href="/FSAI-Site/" title="Startseite">
             <img src="/FSAI-Site/assets/images/ailogo_groß.png">
             Fachschaftsrat Angewandte Informatik
         </a>
-
     </div>
 </nav>
 
