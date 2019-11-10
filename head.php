f131a7472ba30796d4ffbfce8b7b61633efe49a7
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2019-11-10 20:56:32 +0100

    responsive design: eigenes css angelegt

--- head.php
+++ head.php
@@ -1,4 +1,5 @@
 <meta charset="UTF-8">
 <link rel="stylesheet" href="/FSAI-Site/assets/css/design.css">
 <link rel="stylesheet" href="/FSAI-Site/assets/css/navigation.css">
+<link rel="stylesheet" href="/FSAI-Site/assets/css/responsive.css">
 <link rel="shortcut icon" type="image/png" href="/FSAI-Site/assets/images/ailogo_groß.png">
