c483d6572ea173a632ccdb8dadf3dcfbe41139f5
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2019-11-12 21:37:08 +0100

    Bugfix: Responisve Design auf Handy und Tablet gefixt

--- head.php
+++ head.php
@@ -1,4 +1,5 @@
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="/FSAI-Site/assets/css/design.css">
 <link rel="stylesheet" href="/FSAI-Site/assets/css/navigation.css">
 <link rel="stylesheet" href="/FSAI-Site/assets/css/responsive.css">
