2d3a2f0946dabdc8bf32bdd60c7a4a500aab29a1
Niclas <jarowsky.niclas@web.de>
2019-11-14 20:12:35 +0100

    Eventseite bearbeitet

--- pages/events.php
+++ pages/events.php
@@ -15,7 +15,7 @@
             <h1>Unsere Events</h1>
             <div class="ContentEvents">
                 <img src="/FSAI-Site/assets/images/PictureRaster/pictureraster_12.jpg" alt="Eventbild">
-                <a href="#Event'"><h2>Hack Your Environment 2019</h2></a>
+                <h2>Hack Your Environment 2019</h2>
                 <p>Alle Studierenden der Fachrichtung bilden die Fachschaft,
                     aus deren Mitte die Vertreter für den Fachschaftsrat (FSR) gewählt werden.
                     Wird speziell von der Fachschaft (FS) gesprochen, ist meist der Fachschaftsrat gemeint,
@@ -29,7 +29,25 @@
 
             <div class="ContentEvents">
                 <img src="/FSAI-Site/assets/images/PictureRaster/pictureraster_12.jpg" alt="Eventbild">
-                <a href="#Event'"><h2>Hack Your Environment 2019</h2></a>
+                <h2>Hack Your Environment 2019</h2>
+                <p>Alle Studierenden der Fachrichtung bilden die Fachschaft,
+                    aus deren Mitte die Vertreter für den Fachschaftsrat (FSR) gewählt werden.
+                    Wird speziell von der Fachschaft (FS) gesprochen, ist meist der Fachschaftsrat gemeint,
+                    d.h. die Gruppe von Studenten, die sich aktiv für die Interessen der
+                    Studierenden einsetzt.
+                    Die Fachschaft kümmert sich um die Kommunikation der Studenten untereinander
+                    und zwischen der Studenten- und Professorenschaft. Besonderes Engagement gilt
+                    der Gestaltung des studentischen Lebens, sowie der Einflussnahme auf die Entwicklung
+                    der FH und unseres Studienganges durch die Mitarbeit in den Gremien.
+                    Alle Studierenden der Fachrichtung bilden die Fachschaft,
+                    aus deren Mitte die Vertreter für den Fachschaftsrat (FSR) gewählt werden.
+                    Wird speziell von der Fachschaft (FS) gesprochen, ist meist der Fachschaftsrat gemeint,
+                    d.h. die Gruppe von Studenten, die sich aktiv für die Interessen der
+                    Studierenden einsetzt.
+                    Die Fachschaft kümmert sich um die Kommunikation der Studenten untereinander
+                    und zwischen der Studenten- und Professorenschaft. Besonderes Engagement gilt
+                    der Gestaltung des studentischen Lebens, sowie der Einflussnahme auf die Entwicklung
+                    der FH und unseres Studienganges durch die Mitarbeit in den Gremien.</p>
             </div>
         </div>
         <? include '../footer.php';?>
