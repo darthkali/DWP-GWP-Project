979a31d4af4132e7adaa2b845b1df575c6a14be7
Niclas <jarowsky.niclas@web.de>
2019-11-14 20:01:42 +0100

    Eventseite und Footer bearbeitet

--- pages/events.php
+++ pages/events.php
@@ -13,8 +13,22 @@
 
         <div class="Content" id="fadeIn">
             <h1>Unsere Events</h1>
-            <div id="ContentEvents">
-                <img src="/FSAI-Site/assets/images/gruppe.jpg" alt="Eventbild">
+            <div class="ContentEvents">
+                <img src="/FSAI-Site/assets/images/PictureRaster/pictureraster_12.jpg" alt="Eventbild">
+                <a href="#Event'"><h2>Hack Your Environment 2019</h2></a>
+                <p>Alle Studierenden der Fachrichtung bilden die Fachschaft,
+                    aus deren Mitte die Vertreter für den Fachschaftsrat (FSR) gewählt werden.
+                    Wird speziell von der Fachschaft (FS) gesprochen, ist meist der Fachschaftsrat gemeint,
+                    d.h. die Gruppe von Studenten, die sich aktiv für die Interessen der
+                    Studierenden einsetzt.
+                    Die Fachschaft kümmert sich um die Kommunikation der Studenten untereinander
+                    und zwischen der Studenten- und Professorenschaft. Besonderes Engagement gilt
+                    der Gestaltung des studentischen Lebens, sowie der Einflussnahme auf die Entwicklung
+                    der FH und unseres Studienganges durch die Mitarbeit in den Gremien.</p>
+            </div>
+
+            <div class="ContentEvents">
+                <img src="/FSAI-Site/assets/images/PictureRaster/pictureraster_12.jpg" alt="Eventbild">
                 <a href="#Event'"><h2>Hack Your Environment 2019</h2></a>
             </div>
         </div>
