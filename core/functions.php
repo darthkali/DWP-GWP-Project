f67825406bcd8c3341189d1483a186db8c5e6993
Niclas <jarowsky.niclas@web.de>
2019-11-21 16:30:44 +0100

    function sendMail() bearbeitet

--- core/functions.php
+++ core/functions.php
@@ -117,8 +117,8 @@ function sendMail(){
         $header = array();
         $header[] = "MIME-Version: 1.0";
         $header[] = "Content-type: text/plain; charset=utf-8";
-        //$header[] = "From: FSRAI-Kontaktformular <fsraiformular@web.de>";
-        $header[] = "From: FSRAI-Kontaktformular <".$_POST['mail'].">";
+        $header[] = "From: FSRAI-Kontaktformular <fsraiformular@web.de>";
+        //$header[] = "From: FSRAI-Kontaktformular <".$_POST['mail'].">";
 
         $msg = "Gesendet am: " . date("d.m.Y H:i:s") . "\r\nGesendet von: " . $_POST['name'] ."(".$_POST['mail'].")"."\r\n\r\n" . $_POST['text'];
         mail("fsai@fh-erfurt.de", $_POST['subject'], $msg, implode("\r\n", $header));
