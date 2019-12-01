f1a63f455eb6f1766ae44630fc466ecc91d2f6ba
Niclas <jarowsky.niclas@web.de>
2019-12-01 18:29:04 +0100

    -noch ein footer bugfix-->es war ein footer im footer, das hat es um 10px verschoben

--- views/shared/footer.php
+++ views/shared/footer.php
@@ -1,15 +1,14 @@
-<footer>
-    <div id="FooterLeft">
-        <a href="<?=$_SERVER['SCRIPT_NAME']?>/?p=impressum">Impressum</a>
-        <a href="<?=$_SERVER['SCRIPT_NAME']?>/?p=dataProtection">Datenschutz</a>
-    </div>
-    <div id="FooterMiddle">
-        Fachschaftsrat Angewandte Informatik
-    </div>
-    <div id="FooterRight">
-        <a target="_blank" href="https://www.instagram.com"> <img src="/FSAI-Site/assets/images/instagramm.png" class="footer_img" alt="Instagram"></a>
-        <a target="_blank" href="https://www.twitter.com"> <img src="/FSAI-Site/assets/images/twitter.png" class="footer_img" alt="Twitter"></a>
-        <a target="_blank" href="https://www.facebook.com/Fachschaft-Angewandte-Informatik-102233907862322/"> <img src="/FSAI-Site/assets/images/facebook.png" class="footer_img" alt="Facebook"></a>
-    </div>
-</footer>
+<div id="FooterLeft">
+    <a href="<?=$_SERVER['SCRIPT_NAME']?>/?p=impressum">Impressum</a>
+    <a href="<?=$_SERVER['SCRIPT_NAME']?>/?p=dataProtection">Datenschutz</a>
+</div>
+<div id="FooterMiddle">
+    Fachschaftsrat Angewandte Informatik
+</div>
+<div id="FooterRight">
+    <a target="_blank" href="https://www.instagram.com"> <img src="/FSAI-Site/assets/images/instagramm.png" class="footer_img" alt="Instagram"></a>
+    <a target="_blank" href="https://www.twitter.com"> <img src="/FSAI-Site/assets/images/twitter.png" class="footer_img" alt="Twitter"></a>
+    <a target="_blank" href="https://www.facebook.com/Fachschaft-Angewandte-Informatik-102233907862322/"> <img src="/FSAI-Site/assets/images/facebook.png" class="footer_img" alt="Facebook"></a>
+</div>
+
 
