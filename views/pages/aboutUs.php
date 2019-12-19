af05529a6c42754550c2e8b7d03808f46b245530
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2019-12-19 14:12:06 +0100

    Font eingefügt h tags angepasst AboutUs seite überarbeitet

--- views/pages/aboutUs.php
+++ views/pages/aboutUs.php
@@ -1,35 +1,49 @@
 <div class="SitePicture" id="fadeInImg">
     <img class="center" src="<?=ROOTPATH.'assets/images/laptop.jpg'?>" alt="Bild Über Uns Seite">
-
 </div>
 <div class="Content" id="fadeIn">
-    <div class="usPage">
-        <h1>Das sind wir, der Fachschaftsrat!</h1>
-        <div>
+    <div class="aboutUs">
+        <h1>Das sind wir, der Fachschaftsrat!</h1><br>
         <img src="<?=ROOTPATH.'assets/images/campusplan.png'?>">
-            <p> Liebe Studierende, liebe Studieninteressierte,
-                <br><br>
-                wir freuen uns, dass ihr unsere Website gefunden habt.
-                Wir, der Fachschaftsrat Angewandte Informatik, sind die Vertretung für alle Studierenden, die Informatik an der Fachhochschule Erfurt studieren.
-                <br>
-                Wann immer ihr Fragen oder Probleme habt, sind wir mit einem offenen Ohr für euch da und setzen uns für eure Belange ein.
-                Außerdem organisieren wir diverse Veranstaltungen mit, wie z.B. den Wintermarkt, das Bergfest, Hack Your Environment, um euer Studentenleben abwechslungsreicher und spannender zu gestalten.
-                <br>
-                Da wir fast ausschließlich im Sommersemester 2019 in die Fachschaft gewählt wurden, sind wir ein neuer FSR und stehen noch am Anfang unseres Weges. In unseren regelmäßigen Treffen nehmen wir uns organisatorischen Dingen und euren Belangen aller Art an.
-                <br>
-                Auf dieser Website möchten wir euch über unsere Tätigkeiten informieren und euch auf dem Laufenden halten.
-                Für alle Studieninteressierten beantworten wir außerdem alle Fragen rund ums Studium.
-                <br>
-                Wir wünschen euch viel Spaß beim durchstöbern unsere Seite.
-                <br><br>
-                Ihr habt Fragen oder wollt uns einfach mal besuchen?
-                <br>
-                Meistens trefft Ihr uns in der Altonaer Str. 25, 99085 Erfurt, Raum 3.1.23 an.
-                <br><br>
-                Euer FSR Informatik
-                <br>
-            </p>
-        </div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2511.6968035214013!2d11.039306415910632!3d50.98479465659891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a472a47d51a0b7%3A0x16ba89c33fce2610!2sFachhochschule%20Erfurt!5e0!3m2!1sde!2sde!4v1576622523914!5m2!1sde!2sde" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        <p>
+            <h3>Liebe Studierende, liebe Studieninteressierte,</h3>
+            <br>
+            wir freuen uns, dass ihr unsere Website gefunden habt.
+            Wir, der Fachschaftsrat Angewandte Informatik, sind die Vertretung für alle Studierenden, die Informatik an der Fachhochschule Erfurt studieren.
+            <br>
+            Wann immer ihr Fragen oder Probleme habt, sind wir mit einem offenen Ohr für euch da und setzen uns für eure Belange ein.
+            Außerdem organisieren wir diverse Veranstaltungen mit, wie z.B. den Wintermarkt, das Bergfest, Hack Your Environment, um euer Studentenleben abwechslungsreicher und spannender zu gestalten.
+            <br>
+            Da wir fast ausschließlich im Sommersemester 2019 in die Fachschaft gewählt wurden, sind wir ein neuer FSR und stehen noch am Anfang unseres Weges. In unseren regelmäßigen Treffen nehmen wir uns organisatorischen Dingen und euren Belangen aller Art an.
+            <br><br>
+            Auf dieser Website möchten wir euch über unsere Tätigkeiten informieren und euch auf dem Laufenden halten.
+            Für alle Studieninteressierten beantworten wir außerdem alle Fragen rund ums Studium.
+            <br>
+            Wir wünschen euch viel Spaß beim durchstöbern unsere Seite.
+            <br><br>
+            <h4>Euer FSR Angewandte Informatik</h4>
+        </p>
+
+        <br><br>
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2511.6968035214013!2d11.039306415910632!3d50.98479465659891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a472a47d51a0b7%3A0x16ba89c33fce2610!2sFachhochschule%20Erfurt!5e0!3m2!1sde!2sde!4v1576622523914!5m2!1sde!2sde"></iframe>
+
+        <p>
+            <h3>Ihr habt Fragen oder wollt uns einfach mal besuchen?</h3>
+            <br>
+            <h4>Wo findet Ihr uns:</h4>
+            Altonaer Str. 25
+            <br>
+            99085 Erfurt
+            <br>
+            Raum 3.1.23
+            <br><br>
+            <h4>Kontakt aufnehmen:</h4>
+            Tel.:(0361) 6700 752
+            <br>
+            fsai@fh erfurt.de
+            <br>
+            <a href="<?=$_SERVER['SCRIPT_NAME']?>/?c=pages&a=contact">Kontaktformular</a>
+        </p>
     </div>
 </div>
