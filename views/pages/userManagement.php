b83585cf8564dd831af1546672122c4017b7a89a
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2019-12-30 00:35:45 +0100

    refactor userManagement

--- views/pages/userManagement.php
+++ views/pages/userManagement.php
@@ -5,85 +5,75 @@ use FSR_AI\Role;
 
 <div class="Content" id="fadeIn">
     <h1>Nutzerverwaltung</h1>
+
+    <?  // generate Variables for the Members
+    $topic          = 'Mitarbeiter';
+    $ID             = 'MEMBER_ID';
+    $sortMemberUser = 'sortMember';
+    $accounts       = $accountsMember;
+
+    for ($i = 1; $i <= 2; $i++) { ?>
     <table>
         <tr>
-            <th>Mitarbeiter</th>
+            <th><?=$topic?></th>
         </tr>
-        <tr>
-            <? $sortFirstname = (isset($_GET['sortMember']) && $_GET['sortMember'] == 1) ? 2 : 1;?>
-            <? $sortLastname = (isset($_GET['sortMember']) && $_GET['sortMember'] == 3) ? 4 : 3;?>
-            <? $sortBirthDay = (isset($_GET['sortMember']) && $_GET['sortMember'] == 5) ? 6 : 5;?>
-            <? $sortEmail = (isset($_GET['sortMember']) && $_GET['sortMember'] == 7) ? 8 : 7;?>
-            <? $sortRole = (isset($_GET['sortMember']) && $_GET['sortMember'] == 9) ? 10 : 9;?>
-            <? $sortFunction = (isset($_GET['sortMember']) && $_GET['sortMember'] == 11) ? 12 : 11;?>
 
-            <th><a href="?c=user&a=userManagement&sortMember=<?=$sortFirstname?>"> Vorname <i class="fa fa-sort" aria-hidden="true"></i></a></th>
-            <th><a href="?c=user&a=userManagement&sortMember=<?=$sortLastname?>"> Nachname <i class="fa fa-sort" aria-hidden="true"></i></a></th>
-            <th><a href="?c=user&a=userManagement&sortMember=<?=$sortBirthDay?>"> Geburtstag <i class="fa fa-sort" aria-hidden="true"></i></a></th>
-            <th><a href="?c=user&a=userManagement&sortMember=<?=$sortEmail?>"> E-Mail <i class="fa fa-sort" aria-hidden="true"></i></a></th>
-            <th><a href="?c=user&a=userManagement&sortMember=<?=$sortRole?>"> Rolle <i class="fa fa-sort" aria-hidden="true"></i></a></th>
-            <th><a href="?c=user&a=userManagement&sortMember=<?=$sortFunction?>"> Funktion FSR <i class="fa fa-sort" aria-hidden="true"></i></a></th>
+
+        <tr>
+            <?  $sortFirstname  = (isset($_GET[$sortMemberUser]) && $_GET[$sortMemberUser] == 1) ? 2 : 1;
+                $sortLastname   = (isset($_GET[$sortMemberUser]) && $_GET[$sortMemberUser] == 3) ? 4 : 3;
+                $sortBirthDay   = (isset($_GET[$sortMemberUser]) && $_GET[$sortMemberUser] == 5) ? 6 : 5;
+                $sortEmail      = (isset($_GET[$sortMemberUser]) && $_GET[$sortMemberUser] == 7) ? 8 : 7; ?>
+
+            <th><a href="?c=user&a=userManagement&<?=$sortMemberUser?>=<?=$sortFirstname?>"> Vorname <i class="fa fa-sort" aria-hidden="true"></i></a></th>
+            <th><a href="?c=user&a=userManagement&<?=$sortMemberUser?>=<?=$sortLastname?>"> Nachname <i class="fa fa-sort" aria-hidden="true"></i></a></th>
+            <th><a href="?c=user&a=userManagement&<?=$sortMemberUser?>=<?=$sortBirthDay?>"> Geburtstag <i class="fa fa-sort" aria-hidden="true"></i></a></th>
+            <th><a href="?c=user&a=userManagement&<?=$sortMemberUser?>=<?=$sortEmail?>"> E-Mail <i class="fa fa-sort" aria-hidden="true"></i></a></th>
+
+
+            <? if($i === 1){
+                $sortRole = (isset($_GET[$sortMemberUser]) && $_GET[$sortMemberUser] == 9) ? 10 : 9;
+                $sortFunction = (isset($_GET[$sortMemberUser]) && $_GET[$sortMemberUser] == 11) ? 12 : 11;
+                ?>
+                <th><a href="?c=user&a=userManagement&<?=$sortMemberUser?>=<?=$sortRole?>"> Rolle <i class="fa fa-sort" aria-hidden="true"></i></a></th>
+                <th><a href="?c=user&a=userManagement&<?=$sortMemberUser?>=<?=$sortFunction?>"> Funktion FSR <i class="fa fa-sort" aria-hidden="true"></i></a></th>
+            <?}?>
+
             <th>Optionen</th>
         </tr>
-    <?foreach($accountsMember as $index => $account) : ?>
+
+    <?foreach($accounts as $index => $account) : ?>
         <tr>
             <td><?=$account['FIRSTNAME']?></td>
             <td><?=$account['LASTNAME']?></td>
             <td><?=date("d.m.Y", strtotime($account['DATE_OF_BIRTH']));?></td>
             <td><?=$account['EMAIL']?></td>
-            <td><?=Role::generateRoleByRoleID($account['ROLE_ID'])?></td>
-            <td><?=Function_FSR::generateFunctionFSRNameByUserID($account['MEMBER_ID'])?></td>
+            <? if($i === 1){?>
+                <td><?=Role::generateRoleByRoleID($account['ROLE_ID'])?></td>
+                <td><?=Function_FSR::generateFunctionFSRNameByUserID($account[$ID])?></td>
+            <?}?>
             <td>
-                <? if($_SESSION['userId'] <> $account['MEMBER_ID']){?>
-                <a href="?c=user&a=profil&userId=<?=$account['MEMBER_ID']?>">
+                <? if($_SESSION['userId'] <> $account[$ID]){?>
+                <a href="?c=user&a=profil&userId=<?=$account[$ID]?>">
                     <input alt="Edit" type="image" title="User bearbeiten" src=<?=IMAGEPATH.'edit.png'?> ></a>
-                <a href="?c=pages&a=deleteQuestion&userId=<?=$account['MEMBER_ID']?>" onclick="return deleteQuestionUser(this, <?=$account['MEMBER_ID']?>);">
+                <a href="?c=pages&a=deleteQuestion&userId=<?=$account[$ID]?>" onclick="return deleteQuestionUser(this, <?=$account[$ID]?>);">
                     <input alt="Delete" type="image" title="User löschen" src=<?=IMAGEPATH.'entfernen.png'?> ></a>
                 <?}?>
             </td>
         </tr>
-    <?endforeach;?>
+    <?endforeach;
+
+    // generate Variables for the Users
+        $topic          = 'Nutzer';
+        $ID             = 'ID';
+        $accounts       = $accountsUser;
+        $sortMemberUser = 'sortUser';
+
+        ?>
 
     </table>
-
     <br>
-
-    <table>
-        <tr>
-            <th>Nutzer</th>
-        </tr>
-        <tr>
-
-            <? $sortFirstname = (isset($_GET['sortUser']) && $_GET['sortUser'] == 1) ? 2 : 1;?>
-            <? $sortLastname = (isset($_GET['sortUser']) && $_GET['sortUser'] == 3) ? 4 : 3;?>
-            <? $sortBirthDay = (isset($_GET['sortUser']) && $_GET['sortUser'] == 5) ? 6 : 5;?>
-            <? $sortEmail = (isset($_GET['sortUser']) && $_GET['sortUser'] == 7) ? 8 : 7;?>
-            <? $sortRole = (isset($_GET['sortUser']) && $_GET['sortUser'] == 9) ? 10 : 9;?>
-            <? $sortFunction = (isset($_GET['sortUser']) && $_GET['sortUser'] == 11) ? 12 : 11;?>
-
-            <th><a href="?c=user&a=userManagement&sortUser=<?=$sortFirstname?>"> Vorname <i class="fa fa-sort" aria-hidden="true"></i></a></th>
-            <th><a href="?c=user&a=userManagement&sortUser=<?=$sortLastname?>"> Nachname <i class="fa fa-sort" aria-hidden="true"></i></a></th>
-            <th><a href="?c=user&a=userManagement&sortUser=<?=$sortBirthDay?>"> Geburtstag <i class="fa fa-sort" aria-hidden="true"></i></a></th>
-            <th><a href="?c=user&a=userManagement&sortUser=<?=$sortEmail?>"> E-Mail <i class="fa fa-sort" aria-hidden="true"></i></a></th>
-            <th>Optionen</th>
-        </tr>
-        <?foreach($accountsUser as $index => $account) : ?>
-            <tr>
-                <td><?=$account['FIRSTNAME']?></td>
-                <td><?=$account['LASTNAME']?></td>
-                <td><?=date("d.m.Y", strtotime($account['DATE_OF_BIRTH']));?></td>
-                <td><?=$account['EMAIL']?></td>
-                <td>
-                    <? if($_SESSION['userId'] <> $account['ID']){?>
-                        <a href="?c=user&a=profil&userId=<?=$account['ID']?>">
-                            <input alt="Edit" type="image" title="User bearbeiten" src=<?=IMAGEPATH.'edit.png'?> ></a>
-                        <a href="?c=pages&a=deleteQuestion&userId=<?=$account['ID']?>" onclick="return deleteQuestionUser(this, <?=$account['ID']?>)">
-                            <input alt="Delete" type="image"  title="User löschen" src=<?=IMAGEPATH.'entfernen.png'?> ></a>
-                    <?}?>
-                </td>
-            </tr>
-        <?endforeach;?>
-    </table>
+    <? } ?>
 </div>
 
 <script src="<?=JAVASCRIPTPATH.'script.js'?>"></script>
