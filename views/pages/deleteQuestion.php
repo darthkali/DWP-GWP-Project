bd777e36c38d80bb9559ea0a4b7b7e4aafbe355e
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2019-12-30 01:01:34 +0100

    refactor deleteQuestion

--- views/pages/deleteQuestion.php
+++ views/pages/deleteQuestion.php
@@ -7,36 +7,31 @@ use FSR_AI\User;
 </div>
 
 <div class="Content" id="fadeIn">
-
     <img src="<?=IMAGEPATH.'caution.png'?>" alt="ProfilPageImage">
-    <?if(isset($_GET['userId'])){?>
-        <div class="deleteQuestion">
-    <h3>Wollen Sie den Nutzer:</h3>
-    <h1>
-        <?
-        $user = User::findOne('ID = '. $_GET['userId']);
-        echo User::getFullName($user['FIRSTNAME'] , $user['LASTNAME'])
 
-        ?>
-    </h1>
-    <h3> wirklich Löschen?</h3><br>
+    <?if(isset($_GET['userId'])){
+      $title = 'Wollen Sie den Nutzer:';
+        $user = User::findOne('ID = ' . $_GET['userId']);
+        $Name = User::getFullName($user['FIRSTNAME'], $user['LASTNAME']);
+        $hrefTarget = '?c=user&a=userManagement';
+        $hrefDelete = '&userId=' . $_GET['userId'];
+    }else{
+        $title = 'Wollen Sie das Event:';
+        $event = Event::findOne('ID = ' . $_GET['eventId']);
+        $Name = $event['NAME'];
+        $hrefTarget = '?c=event&a=eventManagement';
+        $hrefDelete = '&eventId='. $_GET['eventId'] . '&pictureName='.$_GET['pictureName'];
+    }?>
 
-        <a href="?c=user&a=userManagement&userId=<?= $_GET['userId']?>"><button type="button">Löschen</button></a>
-        <a href="?c=user&a=userManagement"><button type="button">Abbrechen</button></a>
+    <div class="deleteQuestion">
+        <h3><?=$title?></h3>
+        <h1><?=$Name ?></h1>
+        <h3> wirklich Löschen?</h3><br>
+
+        <a href="<?=$hrefTarget?><?=$hrefDelete?>"><button type="button">Löschen</button></a>
+        <a href="<?=$hrefTarget?>"><button type="button">Abbrechen</button></a>
     </div>
-    <?}else{?>
-        <div class="deleteQuestion">
-    <h3>Wollen Sie das Event:</h3>
-    <h1>
-        <?
-        $event = Event::findOne('ID = '. $_GET['eventId']);
-        echo $event['NAME']
-        ?>
-    </h1>
-    <h3> wirklich Löschen?</h3>
 
-        <a href="?c=event&a=eventManagement&eventId=<?=$_GET['eventId']?>&pictureName=<?=$_GET['pictureName']?>"><button type="button">Löschen</button></a>
-        <a href="?c=event&a=eventManagement"><button type="button">Abbrechen</button></a>
-    </div>
-    <?}?>
+
+
 </div>
