fcfd9944c2867d566ec5f84d322af462cde50789
Danny Steinbrecher <46423967+darthkali@users.noreply.github.com>
2020-01-05 10:35:46 +0100

    Fehler beim anzeigen des Bildes im Datenschutz behoben #124

--- views/pages/dataprotection.php
+++ views/pages/dataprotection.php
@@ -1,5 +1,5 @@
 <div class="SitePicture" id="fadeInImg">
- <img class="center" src="--><?//=PAGE_IMAGE_PATH.'programmer.png'?><!--" alt = "Picture Dataprotection"> <!-- TODO: anderes Bild oder Titel-->
+    <img class="center" src="<?=PAGE_IMAGE_PATH.'programmer.png'?>" alt = "Picture Dataprotection"> <!-- TODO: anderes Bild oder Titel-->
 </div>
 <div class="Content" id="fadeIn">
         <h1>Datenschutzerklärung</h1>
